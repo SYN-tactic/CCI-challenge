bffa1d4f66a976b01675cb6c95a5cadc275a9a32
Austen Goodson <austen@goodsons.net>
2021-08-23 13:12:33 -0700

    Create the main app page

--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,27 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import React, { useState } from 'react';
+
 
 export default function Home() {
+  const [text, setText] = useState('Hello');
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Austen Goodson CircleCI App</title>
       </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      <div className="content">
+        <h1>{text}</h1>
+        <button onClick={()=> setText('Hi')}>Say Hi</button>
+      </div>
 
       <footer>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/SYN-tactic"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          Created By{' '} <strong>Austen Goodson</strong>
         </a>
       </footer>
 
@@ -65,18 +31,38 @@ export default function Home() {
           padding: 0 0.5rem;
           display: flex;
           flex-direction: column;
-          justify-content: center;
+          justify-content: space-between;
           align-items: center;
+          background-color: #0e1111;
         }
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
+        .content {
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
+          height: 100%;
+          width: 100%;
+          color: #7F6Df2;
         }
+
+        button {
+          width: 16rem;
+          height: 2rem;
+          border-radius: 8px;
+          background-color: #483699;
+          border: none;
+          color: white;
+          font-weight: bold;
+          font-size: 1rem;
+          letter-spacing: .06rem;
+
+        }
+
+        button:hover {
+          cursor: pointer;
+        }
+
 
         footer {
           width: 100%;
@@ -93,17 +79,22 @@ export default function Home() {
 
         footer a {
           display: flex;
+          flex-direction: column;
           justify-content: center;
           align-items: center;
         }
 
         a {
-          color: inherit;
+          color: #7F6Df2;
           text-decoration: none;
         }
 
+        strong {
+          font-weight: bold;
+          text-decoration: underline;
+        }
+
         .title a {
-          color: #0070f3;
           text-decoration: none;
         }
 
@@ -113,74 +104,6 @@ export default function Home() {
           text-decoration: underline;
         }
 
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
 
         @media (max-width: 600px) {
           .grid {
